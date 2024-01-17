5dbfacdec2408ece7c465e39e4902915c870ccdb
Patryk Wikłacz <wiklacz1patryk@gmail.com>
2024-01-17 16:57:21 +0100

    Home page ended.

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { AppModule } from '../app.module';
 import { CoreModule } from '../core/core.module';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { CoreModule } from '../core/core.module';
   ],
   imports: [
     CommonModule,
-    CoreModule
+    CoreModule,
+    AnimateOnScrollModule,
+    RouterModule
   ],
   exports:[
     HomeComponent
